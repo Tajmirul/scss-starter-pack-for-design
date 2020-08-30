a387d85a25fa92a4b896bc17067a8e3dc323a06a
Tajmirul <tasmirolislam@gmail.com>
2020-08-30 18:41:13 +0600

    Some changes

--- js/main.js
+++ js/main.js
@@ -1,40 +1,73 @@
+$(document).ready(function () {
 
-/* magnificPopup img view */
-$('.popup-image').magnificPopup({
-    type: 'image',
-    gallery: {
-        enabled: true
-    }
-});
+    /* magnificPopup img view */
+    $('.popup-image').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
 
-/* magnificPopup video view */
-$('.popup-video').magnificPopup({
-    type: 'iframe'
-});
+    /* magnificPopup video view */
+    $('.popup-video').magnificPopup({
+        type: 'iframe'
+    });
 
-//for menu active class
-$('.portfolio-menu button').on('click', function (event) {
-    $(this).siblings('.active').removeClass('active');
-    $(this).addClass('active');
-    event.preventDefault();
-});
+    //for menu active class
+    $('.portfolio-menu button').on('click', function (event) {
+        $(this).siblings('.active').removeClass('active');
+        $(this).addClass('active');
+        event.preventDefault();
+    });
 
-// WOW active
-new WOW().init();
+    // WOW active
+    new WOW().init();
 
-$(window).scroll(function () {
-    var scrolled = $(window).scrollTop();
+    // my code ----------------------------------------------------------------------
+    // preloader
+    $(".preloader").animate({
+        top: -2000000
+    }, 1500);
 
-    // go to top button
-    if (scrolled > 100) {
-        $('.go-to-top').addClass('active');
+    // sticky menu ber and go to top button
+    $(window).scroll(function () {
+        var scrolled = $(window).scrollTop();
+        var topMain = $('main').offset().top - 400;
 
-        $('.go-to-top').click(function () {
-            $(window).scrollTop(0);
+        // go to top button
+        if (scrolled > topMain) {
+            $('.go-to-top').addClass('active');
+            $('.nav-outer').addClass('sticky');
+
+            $('.go-to-top').click(function () {
+                $(window).scrollTop(0);
+            });
+        } else {
+            $('.go-to-top').removeClass('active');
+            $('.nav-outer').removeClass('sticky')
+        }
+        // console.log(scrolled);
+    });
+
+    // menu bar open -------------------------
+    $('.navigations .menu-icon').click(function () {
+        $('.mobile-navigations').css({
+            'width': '100%',
+            'padding': '3rem 5rem'
         });
-    } else {
-        $('.go-to-top').removeClass('active');
-    }
-    // console.log(scrolled);
-})
+        // $('.mobile-navigations').toggleClass('w-100 py-5 pl-50 pr-50')
+    });
+    // menu bar close -------------------------
+    $('.mobile-navigations li.close-menu').click(function () {
+        $('.mobile-navigations').css({
+            'width': '0',
+            'padding': '0'
+        });
+    });
+    // dropdown toggle ------------------
+    $('.mobile-navigations li.has-dropdown > i').click(function () {
+        $(this).parent().find('>ul').slideToggle()
+    });
 
+
+});    
